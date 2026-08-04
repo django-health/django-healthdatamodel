0ac5705b8da8beb5a0fbb2bb8372181e4b7dec0b
Andy Reagan <andy@andyreagan.com>
2026-08-03 21:58:47 -0400

    chore: monthly maintenance 2026-08-03

--- healthdatamodel/admin.py
+++ healthdatamodel/admin.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.utils import timezone
 
-
 # ---------------------------------------------------------------------------
 # ---------------------------------------------------------------------------
 
